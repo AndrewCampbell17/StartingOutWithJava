2a070ec87f3fb273d95a4a7fa4d9e7da32f69c63
AndrewCampbell17 <AndrewCampbell85@webster.edu>
2017-02-13 22:12:17 -0600

    Added 2.1 Name, Age, Annual Income

---
+++ src/chapter2/NameAgeIncome.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class NameAgeIncome {
+
+	public static void main(String[] args) {
+		String name = "Andrew Campbell";
+		int age = 22;
+		double annualPay = 100000.00;
+		
+		System.out.println("My name is " + name + ", my age is " + age + " and\n I hope to earn $" + annualPay + " per year");
+
+	}
+
+}
