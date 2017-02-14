842b5c1cd7ead6a96c2cb6009e11d4bb10bb3fe5
AndrewCampbell17 <AndrewCampbell85@webster.edu>
2017-02-13 22:19:31 -0600

    Added 2.2 Name and Initials

---
+++ src/chapter2/NameAndInitials.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class NameAndInitials {
+
+	public static void main(String[] args) {
+		String firstName = "Andrew",
+				middleName = "Jordan",
+				lastName = "Campbell";
+		
+		char firstInitial = 'A',
+				middleInitial = 'J',
+				lastInitial = 'C';
+		
+		System.out.println(firstName + " " + middleName + " " + lastName);
+		System.out.println(firstInitial +""+ middleInitial +""+ lastInitial);
+		
+
+	}
+
+}
